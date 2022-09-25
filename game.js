22ac58870b331a9f2fd353b1046427a71c158373
Subhan Khan <subhank1997@gmail.com>
2022-09-25 18:20:33 -0400

    add variable to change snake speed

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 let lastRenderTime = 0
+const Snake_Speed = 1
 
 function main(currentTime){
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / Snake_Speed) 
+    return 
+    console.log('Rendering')
     lastRenderTime = currentTime
-    //console.log(secondsSinceLastRender)
 }
 window.requestAnimationFrame(main)
