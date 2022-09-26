7a072567c590a8f5ef96765b3c233e3aac9d80d9
Subhan Khan <subhank1997@gmail.com>
2022-09-26 19:04:22 -0400

    added code so while its going in one direction you cant go in the oppisite

--- JS/input.js
+++ JS/input.js
@@ -1,22 +1,32 @@
 let direction = { x:0 , y:0 }
+let lastDirection = { x:0 , y:0 }
 
 window.addEventListener('keydown', e =>{
     switch(e.key){
         case 'ArrowUp':
+        if (lastDirection.y !== 0)
+        break 
             direction = { x:0, y:-1 }
             break
         case 'ArrowDown':
+        if (lastDirection.y !== 0)
+        break
             direction = { x:0, y:1 }
             break
         case 'ArrowLeft':
+        if (lastDirection.x !== 0)
+        break
             direction = { x:-1, y:0 }
             break
         case 'ArrowRight':
+        if (lastDirection.x !== 0)
+        break
             direction = { x:1, y:0 }
             break
     }
 })
 
 export function inputDirection(){
+    lastDirection = direction
     return direction
 }
